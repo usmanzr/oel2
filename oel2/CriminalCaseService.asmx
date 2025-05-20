6b2aed72b8bd380905620819295442ad6900a579
02-131222-124 <02-131222-124@student.bahria.edu.pk>
2025-05-20 10:43:48 +0500

    Add project files.

---
+++ oel2/CriminalCaseService.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="CriminalCaseService.asmx.cs" Class="oel2.CriminalCaseService" %>
---
+++ oel2/CriminalCaseService.asmx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace oel2
+{
+    public class FieldResult
+    {
+        public string FieldName { get; set; }
+        public string Status { get; set; }
+    }
+
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    public class CriminalCaseService : System.Web.Services.WebService
+    {
+        private static readonly List<string> SensitivePatterns = new List<string>
+        {
+            "INF-", "BADGE-", "CASE-SECRET-", "123 Confidential St"
+        };
+
+        [WebMethod]
+        public List<FieldResult> CheckSensitiveData(string caseNumber, string informantID, string badgeID, string address)
+        {
+            return new List<FieldResult>
+            {
+                new FieldResult { FieldName = "CaseNumber", Status = IsSensitive(caseNumber) ? "Sensitive" : "Safe" },
+                new FieldResult { FieldName = "InformantID", Status = IsSensitive(informantID) ? "Sensitive" : "Safe" },
+                new FieldResult { FieldName = "BadgeID", Status = IsSensitive(badgeID) ? "Sensitive" : "Safe" },
+                new FieldResult { FieldName = "Address", Status = IsSensitive(address) ? "Sensitive" : "Safe" }
+            };
+        }
+
+        private bool IsSensitive(string input)
+        {
+            foreach (var pattern in SensitivePatterns)
+            {
+                if (input.StartsWith(pattern))
+                    return true;
+            }
+            return false;
+        }
+    }
+}
